8b6899d1a509b7f7769b10878e9f1461e6397434
Elden Deguia <eldendeguia@outlook.com>
2025-01-04 09:09:45 -0600

    Can add books now

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const myLibrary = [];
 const container = document.querySelector(".container");
+const form = document.querySelector("dialog");
+
+const newButton = document.createElement("button");
+newButton.textContent = "Add Book";
+newButton.addEventListener("click", displayForm);
+container.appendChild(newButton);
+
+form.addEventListener("submit", extractFormValues);
 
 function Book(name, author, pages, isRead) {
     this.name = name;
@@ -19,15 +27,39 @@ function displayBooks() {
     container.appendChild(newButton);
     myLibrary.forEach((book) => {
         const newDiv = document.createElement("div");
-        newDiv.textContent = book.name + book.author + book.pages + book.isRead;
+        newDiv.innerHTML = `
+            Book: ${book.name}<br>
+            Author: ${book.author}<br>
+            Pages: ${book.pages}<br>
+            Is Read: ${book.isRead}<br><br>
+        `;
         container.appendChild(newDiv);
     })
 }
 
-const newButton = document.createElement("button");
-newButton.textContent = "hello";
-newButton.addEventListener("click", () => {
-    addBookToLibrary("joe", "joe", "joe", "joe");
-})
-container.appendChild(newButton);
+function displayForm() {
+    form.showModal();
+}
 
+function extractFormValues(event) {
+    event.preventDefault();
+
+    const enteredName = document.querySelector(`input[name="title"]`);
+    const enteredAuthor = document.querySelector(`input[name="author"]`);
+    const enteredPages = document.querySelector(`input[name="pages"]`);
+    const enteredRead = document.querySelector(`input[type="radio"]:checked`);
+
+    
+
+    addBookToLibrary(enteredName.value, enteredAuthor.value, enteredPages.value, enteredRead.value);
+    enteredName.value = "";
+    enteredAuthor.value = "";
+    enteredPages.value = "";
+    enteredRead.checked = false;
+
+    form.close();
+}
+
+
+
+    
